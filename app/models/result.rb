ced9dae179a9386d35618d2fc194bb47ea787e09
Brooke Martin <brookemartin83@gmail.com>
2019-02-03 22:30:13 -0600

    add validation to prevent duplicate places per game

--- app/models/result.rb
+++ app/models/result.rb
@@ -2,6 +2,6 @@ class Result < ApplicationRecord
   belongs_to :game
   belongs_to :player
 
-  validates :place, presence: true, numericality: true
+  validates :place, presence: true, numericality: true, uniqueness: { scope: :game_id }
   validates :player_id, uniqueness: { scope: :game_id }
 end
